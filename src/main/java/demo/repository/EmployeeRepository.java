94d1b28a2af81fe047f937ee6931fbd971e40d30
dinesh A <dgit2019@gmail.com>
2021-02-24 22:32:03 +0530

    update spring example

---
+++ src/main/java/demo/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import demo.model.EmployeeEntity;
+ 
+@Repository
+public interface EmployeeRepository
+        extends JpaRepository<EmployeeEntity, Long> {
+ 
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('dinesh', 'a', 'dgit2019@gmail.com'),
+  	('j', 'dsecond', 'xyz@email.com');
